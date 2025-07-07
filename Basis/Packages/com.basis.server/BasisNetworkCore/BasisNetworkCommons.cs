38744105aad46fade32ba14c960e9f8b1e693fa7
dooly <doolanl208@gmail.com>
2025-07-07 17:59:47 +1000

    fixed message count

--- Basis/Packages/com.basis.server/BasisNetworkCore/BasisNetworkCommons.cs
+++ Basis/Packages/com.basis.server/BasisNetworkCore/BasisNetworkCommons.cs
@@ -12,7 +12,7 @@ namespace Basis.Network.Core
         /// when adding a new message we need to increase this
         /// will function up to 64
         /// </summary>
-        public const byte TotalChannels = 21;
+        public const byte TotalChannels = 22;
         /// <summary>
         /// channel zero is only used for unreliable methods
         /// we fall it through to stop bugs
