bab4c9098e075dbd0c01475b71c235da0b8966ee
Ån <owo@whatsthis.one>
2025-11-07 22:31:18 -0500

    Fixes scaling math

--- Basis/Packages/com.basis.framework/Interactions/BasisTransform.cs
+++ Basis/Packages/com.basis.framework/Interactions/BasisTransform.cs
@@ -1,23 +1,32 @@
 using UnityEngine;
 namespace Basis.Scripts.BasisSdk.Interactions
 {
+
     public class BasisTransform
     {
-        /// <summary>
-        /// Helper function, scales the object uniformly in all axes, clamped to min/max scale.
-        /// </summary>
-        public static void scaleObjectWithClamp(
+        public enum Direction
+        {
+            Embiggen = 0,
+            Ensmallen = 1
+        }
+
+        public static void ScaleObjectBetween(
             Transform transform,
-            float scaleDirection,
+            BasisTransform.Direction scaleDirection,
+            float stepSize,
             float minScale,
             float maxScale
         )
         {
-            Vector3 vector = Vector3.one;
-            vector *= scaleDirection / 200f;
+            Vector3 scaleVector = Vector3.one;
+            scaleVector *= (scaleDirection == Direction.Embiggen ? 1 : -1) * stepSize;
 
+            BasisDebug.Log(scaleDirection == Direction.Embiggen ? "Embiggening" : "Ensmallening");
+            BasisDebug.Log("Min Scale: " + minScale + " | Max Scale: " + maxScale);
+            BasisDebug.Log(scaleVector.ToString());
             Vector3 newScale = transform.localScale;
-            newScale += vector;
+
+            newScale += scaleVector;
             newScale.x = Mathf.Clamp(newScale.x, minScale, maxScale);
             newScale.y = Mathf.Clamp(newScale.y, minScale, maxScale);
             newScale.z = Mathf.Clamp(newScale.z, minScale, maxScale);
