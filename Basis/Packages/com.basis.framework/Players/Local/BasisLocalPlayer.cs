bb2d6d7b9b6a605c70bf951e35a8a7879c630bac
Aaron Franke <arnfranke@yahoo.com>
2025-11-10 15:32:16 -0800

    Replace Has bools in BasisLocalBoneControl
    This design makes it it is impossible for this class to end up in an invalid state (at least with regard to these bools).

--- Basis/Packages/com.basis.framework/Players/Local/BasisLocalPlayer.cs
+++ Basis/Packages/com.basis.framework/Players/Local/BasisLocalPlayer.cs
@@ -303,9 +303,10 @@ namespace Basis.Scripts.BasisSdk.Players
         public void Teleport(Vector3 position, Quaternion rotation)
         {
             BasisDebug.Log("Teleporting", BasisDebug.LogTag.Local);
+            bool wasCharacterEnabled = LocalCharacterDriver.IsEnabled;
             LocalCharacterDriver.IsEnabled = false;
             this.transform.SetPositionAndRotation(position, rotation);
-            LocalCharacterDriver.IsEnabled = true;
+            LocalCharacterDriver.IsEnabled = wasCharacterEnabled;
             LocalAnimatorDriver.HandleTeleport();
             OnSpawnedEvent?.Invoke();
         }
@@ -422,6 +423,7 @@ namespace Basis.Scripts.BasisSdk.Players
             LocalCharacterDriver.SimulateMovement(DeltaTime);
 
             // moves all bones to where they belong
+            // This also drives head and camera movement.
             LocalBoneDriver.SimulateAndApply(this, DeltaTime);
 
             // moves Avatar Hip Transform to where it belongs in tpose.
