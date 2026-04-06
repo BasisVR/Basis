c79c29b2119e038373d079274fbf09ace2c42d11
Toys0125 <marcus.g.stange@gmail.com>
2026-04-06 01:50:48 -0500

    Missed one.

--- Basis/Packages/com.basis.bundlemanagement/BasisBundleLoadAsset.cs
+++ Basis/Packages/com.basis.bundlemanagement/BasisBundleLoadAsset.cs
@@ -108,7 +108,7 @@ public static class BasisBundleLoadAsset
                     BasisDebug.Log("Scene set as active: " + loadedScene.name);
                 }
                 BasisDebug.Log("Scene loaded: " + loadedScene.name + " (MakeActive=" + MakeActiveScene + ", Incremented=" + AssignedIncrement + ")");
-#if UNITY_SERVER && UNITY_BUNDLEUNLOAD
+#if UNITY_BUNDLEUNLOAD
                 bundle.ReleaseBundleBackingStore();
 #endif
                 progressCallback.ReportProgress(UniqueID, 100, "loading scene"); // Set progress to 100 when done
