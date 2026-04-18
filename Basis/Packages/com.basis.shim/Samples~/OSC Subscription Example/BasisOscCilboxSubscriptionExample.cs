74078e1e7222af43d72300cb7ec59c237a1ce5f7
Toys0125 <marcus.g.stange@gmail.com>
2026-04-17 22:12:55 -0500

    Add sample and Example script of osc subscription.

---
+++ Basis/Packages/com.basis.shim/Samples~/OSC Subscription Example/BasisOscCilboxSubscriptionExample.cs
@@ -0,0 +1,61 @@
+using Cilbox;
+using HVR.Basis.Comms.OSC;
+using UnityEngine;
+
+namespace Basis.Shims.Samples
+{
+    [Cilboxable]
+    [RequireComponent(typeof(Basis.Shims.BasisOsc))]
+    public class BasisOscCilboxSubscriptionExample : MonoBehaviour
+    {
+        private const string ExplicitTestAddress = "/avatar/parameters/test";
+        private const string ImplicitTestAddress = "test";
+
+        private Basis.Shims.BasisOsc osc;
+
+        private void Awake()
+        {
+            osc = GetComponent<Basis.Shims.BasisOsc>();
+        }
+
+        private void OnEnable()
+        {
+            if (osc == null)
+            {
+                Debug.LogError("BasisOscCilboxSubscriptionExample requires a BasisOsc component.");
+                return;
+            }
+
+            osc.Subscribe(ExplicitTestAddress, OnExplicitTestTriggered);
+            osc.Subscribe(ImplicitTestAddress, OnImplicitTestTriggered);
+
+            Debug.Log("Subscribed to " + ExplicitTestAddress + " and implicit \"" + ImplicitTestAddress + "\".");
+        }
+
+        private void OnDisable()
+        {
+            if (osc == null)
+            {
+                return;
+            }
+
+            osc.Unsubscribe(ExplicitTestAddress, OnExplicitTestTriggered);
+            osc.Unsubscribe(ImplicitTestAddress, OnImplicitTestTriggered);
+        }
+
+        private void OnExplicitTestTriggered(OscMessage message, OscData[] arguments)
+        {
+            Debug.Log("Explicit OSC subscription fired for " + message.Path + " with " + GetArgumentCount(arguments) + " argument(s).");
+        }
+
+        private void OnImplicitTestTriggered(OscMessage message, OscData[] arguments)
+        {
+            Debug.Log("Implicit OSC subscription fired for " + message.Path + " with " + GetArgumentCount(arguments) + " argument(s).");
+        }
+
+        private static int GetArgumentCount(OscData[] arguments)
+        {
+            return arguments == null ? 0 : arguments.Length;
+        }
+    }
+}
