49d7d061ed7588cc38f8584604475e234a4cfc9f
Toys0125 <marcus.g.stange@gmail.com>
2026-04-30 15:37:18 -0500

    Add GUID to BasisAvatar. May need to increment server number.

--- Basis/Packages/com.basis.framework/Networking/BasisNetworkConnection.cs
+++ Basis/Packages/com.basis.framework/Networking/BasisNetworkConnection.cs
@@ -1,5 +1,6 @@
 using Basis.BasisUI;
 using Basis.Network.Core;
+using Basis.Scripts.Avatar;
 using Basis.Scripts.BasisSdk.Players;
 using Basis.Scripts.Device_Management;
 using Basis.Scripts.Drivers;
@@ -46,6 +47,16 @@ namespace Basis.Scripts.Networking
             BNL.LogErrorOutput += LogErrorOutput;
 
             var uuid = BasisDIDAuthIdentityClient.GetOrSaveDID();
+            var basisLocalPlayer = BasisLocalPlayer.Instance;
+            basisLocalPlayer.UUID = uuid;
+
+            if (!BasisAvatarFactory.EnsureAvatarNetworkGUID(basisLocalPlayer.BasisAvatar, out string avatarNetworkGuid))
+            {
+                BasisDebug.LogError("Unable to start network connection because the local avatar has no network GUID.", BasisDebug.LogTag.Networking);
+                return;
+            }
+
+            byte[] avatarBytes = BasisBundleConversionNetwork.ConvertBasisLoadableBundleToBytes(basisLocalPlayer.AvatarMetaData);
 
             if (isHostMode)
             {
@@ -66,11 +77,6 @@ namespace Basis.Scripts.Networking
 
             BasisDebug.Log($"Connecting with Port {port} IpString {ipString}");
 
-            var basisLocalPlayer = BasisLocalPlayer.Instance;
-            basisLocalPlayer.UUID = uuid;
-
-            byte[] avatarBytes = BasisBundleConversionNetwork.ConvertBasisLoadableBundleToBytes(basisLocalPlayer.AvatarMetaData);
-
             var readyMessage = new ReadyMessage
             {
                 clientAvatarChangeMessage = new ClientAvatarChangeMessage
@@ -78,6 +84,7 @@ namespace Basis.Scripts.Networking
                     byteArray = avatarBytes,
                     loadMode = basisLocalPlayer.AvatarLoadMode,
                     LocalAvatarIndex = 0,
+                    AvatarNetworkGuid = avatarNetworkGuid,
                 },
                 playerMetaDataMessage = new ClientMetaDataMessage
                 {
